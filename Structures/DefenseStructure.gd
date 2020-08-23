84cc096fbda966a20cc309bca3ab1d51f42b8e6d
Sean LaPlante <laplante.sean@gmail.com>
2020-08-22 23:45:49 -0400

    Basic upgrade

--- Structures/DefenseStructure.gd
+++ Structures/DefenseStructure.gd
@@ -4,6 +4,8 @@ class_name DefenseStructure
 signal sold(value)
 
 export(int) var STRUCTURE_VALUE = 50
+export(int) var UPGRADE_STARTING_COST = 50
+export(int) var UPGRADE_INFLATION = 50
 export(float) var ATTACK_COOLDOWN_TIME = 0.25
 
 enum StructureState {
@@ -12,6 +14,7 @@ enum StructureState {
 }
 
 var state = StructureState.SEARCH
+var upgrade_cost = 0
 
 onready var baseSprite = $Base
 onready var bodySprite = $Body
@@ -20,6 +23,10 @@ onready var muzzle = $Body/Muzzle
 onready var attackCooldown = $AttackCooldown
 onready var sellIcon = $SellIcon
 onready var upgradeIcon = $UpgradeIcon
+
+
+func _ready():
+	upgrade_cost = UPGRADE_STARTING_COST
 
 
 func _physics_process(delta):
@@ -55,4 +62,9 @@ func _on_SellIcon_icon_clicked(structure):
 
 
 func _on_UpgradeIcon_icon_clicked(structure):
-	print("Upgrade!")
+	if upgrade_cost > LevelStats.money:
+		return
+	LevelStats.money -= upgrade_cost
+	STRUCTURE_VALUE += upgrade_cost
+	upgrade_cost += UPGRADE_INFLATION
+	ATTACK_COOLDOWN_TIME -= 0.05
